4da6368ee110495e978852c217941b926eccbe40
Aivin J Chiramal <110657004+AivinJoy@users.noreply.github.com>
2025-06-12 22:51:46 +0530

    Add files via upload
    About us section develop

--- script.js
+++ script.js
@@ -22,3 +22,38 @@ document.querySelectorAll('.video-box').forEach(box => {
         }
     });
 });
+
+
+// Counter 
+
+
+const counter = document.getElementById("years");
+const project = document.getElementById("projects");
+const customer = document.getElementById("customers");
+const startYear = 2015;
+const currentYear = new Date().getFullYear();
+const yearsPassed = currentYear - startYear;
+const projectTarget = 25;
+
+// animation
+
+let count1 = 0;
+let count2 = 0;
+const speed = 100;
+const interval = setInterval(() => {
+    if(count1 <= yearsPassed || count2 <= projectTarget) {
+        if(count1 < yearsPassed) {
+            count1++;
+            counter.textContent = count1;
+        }
+        if(count2 < projectTarget) {
+            count2++;
+            project.textContent = `${count2} +`
+            customer.textContent = `${count2} +`
+        }
+    }    
+    else {
+        clearInterval(interval);
+    }
+},speed);
+
